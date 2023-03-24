78eadcf7c657e025a9e5bfcd63b1b4eb6d58d163
Adiul Alam Adil <adiulalam@gmail.com>
2023-03-24 03:09:58 +0000

    jwt

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -2,6 +2,16 @@ const express = require("express");
 const { checkNoteValidId, checkNoteValidData } = require("../middleware/note.middleware");
 const router = express.Router();
 const notes = require("../controller/note.controller");
+const { expressjwt: jwt } = require("express-jwt");
+const _ = require("lodash");
+
+router.use(jwt({ secret: "1234", algorithms: ["HS256"] }), function (req, res, next) {
+	if (!req.auth.admin) {
+		return res.sendStatus(401).send(JSON.stringify({ message: "Unauthorized user" }));
+	} else {
+		next();
+	}
+});
 
 router.route("/").get(notes.fetchAll).post(checkNoteValidData, notes.createByID);
 
