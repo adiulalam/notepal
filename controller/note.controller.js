afaa99b4ef29e9214c7cadd3e5bbbd140671a3f1
Adiul Alam Adil <adiulalam@gmail.com>
2023-04-01 02:22:22 +0100

    notes controller

--- controller/note.controller.js
+++ controller/note.controller.js
@@ -13,19 +13,31 @@ const fetchAll = (req, res) => {
 
 const fetchByID = (req, res) => {
 	const { id } = req.params;
-	connection.query(
-		"select note_id, note_title, note_description, is_archived from note where note_id=?",
-		[id],
-		function (error, results) {
-			if (error) throw error;
-			res.end(JSON.stringify(results));
-		}
-	);
+	const { id: JwtID, is_admin } = req.auth;
+
+	let select = "select note_id, note_title, note_description, is_archived";
+	let from = "from note";
+	let where = "where note_id=?";
+	const queryVar = [parseInt(id)];
+
+	if (!is_admin) {
+		from += " INNER JOIN user ON user.user_id = note.fk_user_id ";
+		where += " AND user_id=? ";
+		queryVar.push(JwtID);
+	}
+
+	const query = `${select} ${from} ${where}`;
+
+	connection.query(query, queryVar, function (error, results) {
+		if (error) throw error;
+		res.end(JSON.stringify(results));
+	});
 };
 
 //Create new note
 const createNote = (req, res) => {
-	const { note_title, note_description, is_archived, fk_user_id } = req.body;
+	const { note_title, note_description, is_archived } = req.body;
+	const { id: fk_user_id } = req.auth;
 
 	connection.query(
 		"INSERT INTO note (note_title, note_description, is_archived, fk_user_id) VALUES (?, ?, ?, ?)",
@@ -40,23 +52,41 @@ const createNote = (req, res) => {
 //update note by id
 const updateByID = (req, res) => {
 	const { id } = req.params;
-	const { note_title, note_description, is_archived, fk_user_id } = req.body;
+	const { note_title, note_description, is_archived } = req.body;
+	const { id: fk_user_id, is_admin } = req.auth;
 
-	connection.query(
-		"UPDATE note SET note_title=?, note_description=?, is_archived=?, fk_user_id=? where note_id=?",
-		[note_title, note_description, is_archived, fk_user_id, id],
-		function (error, results) {
-			if (error) throw error;
-			res.end(JSON.stringify(results));
-		}
-	);
+	let update = "UPDATE note SET note_title=?, note_description=?, is_archived=?";
+	let where = "WHERE note_id=?";
+	const queryVar = [note_title, note_description, is_archived, parseInt(id)];
+
+	if (!is_admin) {
+		where += " AND fk_user_id=? ";
+		queryVar.push(fk_user_id);
+	}
+	const query = `${update} ${where}`;
+
+	connection.query(query, queryVar, function (error, results) {
+		if (error) throw error;
+		res.end(JSON.stringify(results));
+	});
 };
 
 //delete note by id
 const deleteByID = (req, res) => {
 	const { id } = req.params;
+	const { id: fk_user_id, is_admin } = req.auth;
 
-	connection.query("DELETE FROM note WHERE note_id=?", [id], function (error, results) {
+	let deleteQuery = "DELETE FROM note";
+	let where = "WHERE note_id=?";
+	const queryVar = [parseInt(id)];
+
+	if (!is_admin) {
+		where += " AND fk_user_id=? ";
+		queryVar.push(fk_user_id);
+	}
+	const query = `${deleteQuery} ${where}`;
+
+	connection.query(query, queryVar, function (error, results) {
 		if (error) throw error;
 		res.end(JSON.stringify(results));
 	});
@@ -65,33 +95,49 @@ const deleteByID = (req, res) => {
 //update note by id for archive
 const updateByIDForArchive = (req, res) => {
 	const { id } = req.params;
-	connection.query(
-		"UPDATE note SET is_archived=true where note_id=?",
-		[id],
-		function (error, results) {
-			if (error) throw error;
-			res.end(JSON.stringify(results));
-		}
-	);
+	const { id: fk_user_id, is_admin } = req.auth;
+
+	let update = "UPDATE note SET is_archived=true";
+	let where = "WHERE note_id=?";
+	const queryVar = [parseInt(id)];
+
+	if (!is_admin) {
+		where += " AND fk_user_id=? ";
+		queryVar.push(fk_user_id);
+	}
+	const query = `${update} ${where}`;
+
+	connection.query(query, queryVar, function (error, results) {
+		if (error) throw error;
+		res.end(JSON.stringify(results));
+	});
 };
 
 //update note by id for archive
 const updateByIDForNotArchive = (req, res) => {
 	const { id } = req.params;
-	connection.query(
-		"UPDATE note SET is_archived=false where note_id=?",
-		[id],
-		function (error, results) {
-			if (error) throw error;
-			res.end(JSON.stringify(results));
-		}
-	);
+	const { id: fk_user_id, is_admin } = req.auth;
+
+	let update = "UPDATE note SET is_archived=false";
+	let where = "WHERE note_id=?";
+	const queryVar = [parseInt(id)];
+
+	if (!is_admin) {
+		where += " AND fk_user_id=? ";
+		queryVar.push(fk_user_id);
+	}
+	const query = `${update} ${where}`;
+
+	connection.query(query, queryVar, function (error, results) {
+		if (error) throw error;
+		res.end(JSON.stringify(results));
+	});
 };
 
 //Get all notes which are archived
 const fetchAllIsArchived = (req, res) => {
 	connection.query(
-		"select note_id, note_title, note_description, is_archived from note where is_archived = true;",
+		"select note_id, note_title, note_description, is_archived from note where is_archived = true",
 		function (error, results) {
 			if (error) throw error;
 			res.end(JSON.stringify(results));
@@ -101,7 +147,7 @@ const fetchAllIsArchived = (req, res) => {
 
 const fetchAllIsNotArchived = (req, res) => {
 	connection.query(
-		"select note_id, note_title, note_description, is_archived from note where is_archived = false;",
+		"select note_id, note_title, note_description, is_archived from note where is_archived = false",
 		function (error, results) {
 			if (error) throw error;
 			res.end(JSON.stringify(results));
--- middleware/note.middleware.js
+++ middleware/note.middleware.js
@@ -3,12 +3,7 @@ const _ = require("lodash");
 const checkNoteValidData = (req, res, next) => {
 	const { note_title, note_description, is_archived, fk_user_id } = req.body;
 
-	if (
-		_.isNil(note_title) ||
-		_.isNil(note_description) ||
-		_.isNil(is_archived) ||
-		_.isNil(fk_user_id)
-	) {
+	if (_.isNil(note_title) || _.isNil(note_description) || _.isNil(is_archived)) {
 		return res.status(400).send(JSON.stringify({ message: "Missing Fields" }));
 	} else {
 		next();
