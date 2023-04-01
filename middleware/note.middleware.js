1095492c7a6aea07b7695baf4a8b318b9a14f152
Adiul Alam Adil <adiulalam@gmail.com>
2023-04-01 02:41:54 +0100

    cleanup

--- middleware/note.middleware.js
+++ middleware/note.middleware.js
@@ -1,7 +1,7 @@
 const _ = require("lodash");
 
 const checkNoteValidData = (req, res, next) => {
-	const { note_title, note_description, is_archived, fk_user_id } = req.body;
+	const { note_title, note_description, is_archived } = req.body;
 
 	if (_.isNil(note_title) || _.isNil(note_description) || _.isNil(is_archived)) {
 		return res.status(400).send(JSON.stringify({ message: "Missing Fields" }));
